61235d551e60deaa221625eca2ac9c8492420bae
xule05 <xule05@meituan.com>
2020-03-07 16:44:30 +0800

    前端UI优化

--- src/main/resources/static/back/js/app.js
+++ src/main/resources/static/back/js/app.js
@@ -1,33 +1,3 @@
-// logout
-$("#logout").on(ace.click_event, function() {
-    bootbox.confirm("确定退出?", function(result) {
-        if(result) {$.ajax({url: "/user/logout/", type: "GET", success: function (response) {window.location.reload();}});}
-    });
-});
-
-var showSuccessNotify = function showSuccessNotify(title, message) {
-    PNotify.prototype.options.styling = "bootstrap3";
-    new PNotify({
-        title: title,
-        text: message,
-        hide: true,
-        delay: 3000,
-        sticker: false,
-        addclass: 'pnotify-light',
-        type: 'success'
-    });
-};
-
-var showErrorNotiy = function (title, message) {
-    PNotify.prototype.options.styling = "bootstrap3";
-    new PNotify({title: title, text: message, delay: 3000, icon: "fa fa-exclamation-triangle", type: 'error'});
-};
-
-var showNoticeNotiy = function (title, message) {
-    PNotify.prototype.options.styling = "bootstrap3";
-    new PNotify({title: title, text: message, delay: 3000, icon: "fa fa-exclamation-triangle", type: 'notice'});
-};
-
 // ajax 请求数据补全输入框
 $("#parameter").typeahead({
     source: function (query, process) {
@@ -107,3 +77,76 @@ function appendToTable(tmp) {
     );
     return body;
 }
+
+// 参数诊断
+$("#parameters").change(function () {
+    let values = $("#parameters").val();
+    let jdkVersion = $("input[name='jdkVersion']:checked").val();
+    let totalMem = $("input[name='totalMem']:checked").val();
+    $.ajax({
+        type: "POST", //请求方式
+        url: "/evaluate",  //请求地址
+        contentType: "application/x-www-form-urlencoded;charset=UTF-8",  //请求的媒体类型
+        data: {"parameters": values, "jdkVersion": jdkVersion, "totalMem": totalMem},    //参数值
+        success: function (result) {  //请求成功
+            showSuccessNotify("success", "参数诊断完成");
+            $("#result2").show();
+            $("#result2-table > tbody").empty();
+            let errcount=0,warncount=0,okcount=0;
+            for (let i = 0; i < result.length; i++) {
+                if (result[i].level==="OK"){
+                    okcount++;
+                } else if (result[i].level==="WARN"){
+                    warncount++;
+                }else if (result[i].level==="ERROR"){
+                    errcount++;
+                }
+                let tr = appendToTable2(result[i], i);
+                $("#result2-table > tbody").append($(tr));
+            }
+            $("#totalResult").text("发现ERROR级别参数有"+errcount+"个,发现WARN级别的参数有"+warncount+"个,配置正确的参数有"+okcount+"个");
+        },
+        error: function (e) {//请求失败，包含具体的错误信息
+            alert("err");
+        }
+    });
+});
+
+function appendToTable2(tmp, indedx) {
+    let level = tmp.level;
+    let category = tmp.category;
+    let title = tmp.title;
+    let conent = tmp.conent;
+    let tips = tmp.tips;
+    let tr = $('<tr>\n' +
+        '      <th scope="row">' + indedx + '</th>\n' +
+        '      <td>' + level + '</td>\n' +
+        '      <td>' + category + '</td>\n' +
+        '      <td>' + title + '</td>\n' +
+        '      <td>' +
+        '          <div class="btn-group">' +
+        '             <button class="btn btn-xs btn-info center" onclick="showDetail(this)"> >>> </button>' +
+        '         <div>' +
+        '      </td>' +
+        '      <td class="center hidden">' + conent + '</td>' +  // 该列隐藏，由模态框触发
+        '      <td class="center hidden">' + tips + '</td>' +  // 该列隐藏，由模态框触发
+        '   </tr>\n');
+    return tr;
+}
+
+var modelText1;
+var modelText2;
+
+function showDetail(obj) {
+    var thisObj = $(obj);//js对象转jquery对象 关键
+    var td = thisObj.closest("td");
+    modelText1 = td.next().text();
+    modelText2 = td.next().next().text();
+    $('#qrcode').modal('show');
+}
+
+$('#qrcode').on('show.bs.modal', function (event) {
+    var modal = $(this);  //get modal itself
+    modal.find('.modal-body #modelText1').text(modelText1);
+    modal.find('.modal-body #modelText2').text(modelText2);
+});
